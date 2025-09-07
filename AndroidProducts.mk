cf18957d08f66a44d4ac9efe0dd65e60405cf579
LMO GerritBot <gerrit@libremobileos.com>
2025-09-07 13:05:32 +0000

    Adapt to LMODroid by device-add script

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_odroidc4.mk
+    $(LOCAL_DIR)/lmodroid_odroidc4.mk
--- lmodroid_odroidc4.mk
+++ lmodroid_odroidc4.mk
@@ -11,7 +11,7 @@ PRODUCT_IS_ATV := true
 $(call inherit-product, device/google/atv/products/atv_base.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_tv.mk)
+$(call inherit-product, vendor/lmodroid/config/common_full_tv.mk)
 
 # Inherit device configuration
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -22,7 +22,7 @@ PRODUCT_DEVICE := odroidc4
 PRODUCT_GMS_CLIENTID_BASE := android-askey-tv
 PRODUCT_MANUFACTURER := hardkernel
 PRODUCT_MODEL := odroid c4
-PRODUCT_NAME := lineage_odroidc4
+PRODUCT_NAME := lmodroid_odroidc4
 
 PRODUCT_GMS_CLIENTID_BASE := android-droid-tv
 
